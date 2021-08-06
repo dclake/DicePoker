907fc67161c3c0715f90782e191fd6d95e710591
D.C. Lake <dclake@gmail.com>
2021-08-06 19:17:32 -0400

    Working on Logic For Dice Rooll

--- src/DicePoker/App.java
+++ src/DicePoker/App.java
@@ -6,8 +6,9 @@ public class App {
 
 	public static void main(String[] args) {
 //Declare variables
-		int startBalance = 6, betCost = 1, betTotal = 5;
-		int bankBalance, betAmount = 0, placeBet = 1;
+		int startBalance = 6, betCost = 1, betTotal = 5,betAmount = 0, placeBet = 1;
+		int bankBalance, rollOne;
+		int []dieOne = {1,2,3,4,5,6};
 		
 		String betAsString;
 		
@@ -15,14 +16,22 @@ public class App {
 		bankBalance=startBalance;
 		JOptionPane.showMessageDialog(null,showBalance(bankBalance));
 		
-		placeBet = JOptionPane.showConfirmDialog (null, "Would you like to place a bet for $1","Place Bet", placeBet);
-		if(placeBet == JOptionPane.YES_OPTION) { 
-		    betAmount = 1;
-		        if(placeBet == JOptionPane.NO_OPTION) {
-		         
-		        }
-		}
-		JOptionPane.showMessageDialog(null, betAmount);
+		rollOne = dieOne[(int) (Math.random()*6)];
+		JOptionPane.showMessageDialog(null,rollOne);
+		
+		rollOne = dieOne[(int) (Math.random()*6)];
+		JOptionPane.showMessageDialog(null,rollOne);
+
+
+		
+//		placeBet = JOptionPane.showConfirmDialog (null, "Would you like to place a bet for $1","Place Bet", placeBet);
+//		if(placeBet == JOptionPane.YES_OPTION) { 
+//		    betAmount = 1;
+//		        if(placeBet == JOptionPane.NO_OPTION) {
+//		         
+//		        }
+//		}
+//		JOptionPane.showMessageDialog(null, betAmount);
 		
 
 	}
@@ -31,5 +40,7 @@ public class App {
 		return balanceMessage;
 				
 	}
+	
+	
 
 }
