0b84bc891b45e907ca9e54a4694267822a9e2ab1
dave.lake <dclake@gmail.com>
2021-08-06 15:53:50 -0400

    First commit

---
+++ src/DicePoker/App.java
@@ -0,0 +1,14 @@
+package DicePoker;
+public class App {
+
+	public static void main(String[] args) {
+//Declare variables
+		int startBalance = 6, betCost = 1, betTotal = 5;
+		int bankBalance;
+		
+		
+		
+
+	}
+
+}
