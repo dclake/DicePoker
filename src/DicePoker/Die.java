9695921c0a1479c376f8a439cc856840715cc94d
D.C. Lake <dclake@gmail.com>
2021-08-07 13:59:41 -0400

    Created variables in die class for random in roll method

--- src/DicePoker/Die.java
+++ src/DicePoker/Die.java
@@ -2,15 +2,11 @@ package DicePoker;
 
 public class Die {
 	private static int dieValue;
+	private static int smallestDieSide = 1, largestDieSide = 6;
 	
-	
-	public int getDieValue() {
-		return dieValue;
-	}
 	
 	public static int roll() {
-		dieValue = (int)(Math.random()*6)+1;
-//		face = face++;
+		dieValue = (int)(Math.random()* largestDieSide)+ smallestDieSide;
 		return dieValue;
 	}
 	
