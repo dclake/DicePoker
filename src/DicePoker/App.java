63c785496319f85b051cfe85031554fdaba20e68
D.C. Lake <dclake@gmail.com>
2021-08-13 00:22:49 -0400

    Working on updating bank balance

--- src/DicePoker/App.java
+++ src/DicePoker/App.java
@@ -5,7 +5,7 @@ import javax.swing.JOptionPane;
 public class App {
 
 	public static void main(String[] args) {
-//Declare variables
+		//Declare variables
 		int startBalance = 6, betCost = 1, betTotal = 5,betAmount = 1, placeBet = 1;
 		int betCount = 0;
 		int bankBalance, rollOne, rollTwo, winnings;
@@ -18,11 +18,7 @@ public class App {
 		Player p1 = new Player();
 		
 
-//		dice.roll();
-//		dice.checkSequential();
-//		dice.checkEqual();
-//        System.out.println("");
-//        dice.checkRollResult();
+
 
 				
 		
@@ -41,46 +37,26 @@ public class App {
 			balanceString = p1.playerBalanceString();
 			balanceString = balanceString + "\nWould you like to place a bet of $1?";
 //			
-			//JOptionP
-			placeBet = JOptionPane.showConfirmDialog (null, "Would you like to place a bet for $1","Place Bet", placeBet);
-					rollDice();
-//		
-//		if (rollOne == rollTwo) {
-//			winnings = betAmount *3;
-//			p1.setBankBalance(bankBalance + winnings);
-//			output = "You won!!! \nYou earn tripple your bet";
-//			JOptionPane.showMessageDialog(null,output);
-//		}
-//		else {
-//			if (rollOne - rollTwo == 1|| rollTwo - rollOne == 1) {
-//				winnings = betAmount *2;
-//				p1.setBankBalance(bankBalance + winnings);
-//				output = "You won!!! \nYou earn double your bet";
-//				JOptionPane.showMessageDialog(null,output);
-//			}
-//			else {
-//			output = "You Loose!!!";
-//			p1.setBankBalance(bankBalance -1);
-//			JOptionPane.showMessageDialog(null,output);
-//			}
-//		}
-//		++betCount;
+			placeBet = JOptionPane.showConfirmDialog (null, balanceString,"Place Bet", placeBet);
+			
+			winnings = rollDice();
+			p1.setBankBalance(p1.getBankBalance()+ winnings);
 		}
-//
-//		JOptionPane.showMessageDialog(null,showBalance(bankBalance));
-//
-//		
-//		
+
 		
 
 	}
-	public static void rollDice() {
+	public static int rollDice() {
 		Dice dice = new Dice();
 		
 		dice.roll();
 		dice.checkRollResult();
 	String rollResults = dice.readDice();
 	JOptionPane.showMessageDialog(null,rollResults);
+	int winnings = 1;
+	return winnings;
+	
+	
 
 	}
 	public static String showBalance(int bankBalance) {
